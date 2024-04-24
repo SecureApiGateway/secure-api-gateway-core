87da431f0ce74fa43d2e05c3a4a668a898c46d54
Dan Badham <dan.badham@forgerock.com>
2024-04-24 16:05:15 +0100

    1115: Remove blocking IO call in TokenEndpointMetricsContextSupplier
    MetricsContextSupplier interface now returns a Promise.
    
    TokenEndpointMetricsContextSupplier accesses the Request's Form entity asynchronously, which removes the blocking IO call.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1115

--- secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/metrics/RouteMetricsFilter.java
+++ secure-api-gateway-ig-extensions/src/main/java/com/forgerock/sapi/gateway/metrics/RouteMetricsFilter.java
@@ -15,8 +15,9 @@
  */
 package com.forgerock.sapi.gateway.metrics;
 
+import static java.util.Objects.requireNonNull;
+import static java.util.Objects.requireNonNullElse;
 import static org.forgerock.openig.util.JsonValues.optionalHeapObject;
-import static org.forgerock.util.Reject.checkNotNull;
 
 import java.util.Collections;
 import java.util.Map;
@@ -34,6 +35,7 @@ import org.forgerock.openig.heap.HeapException;
 import org.forgerock.services.context.Context;
 import org.forgerock.util.promise.NeverThrowsException;
 import org.forgerock.util.promise.Promise;
+import org.forgerock.util.promise.Promises;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -82,22 +84,23 @@ public class RouteMetricsFilter implements Filter {
     public RouteMetricsFilter(Ticker ticker, LongSupplier timestampSupplier,
                               RouteMetricsEventPublisher metricsEventPublisher,
                               MetricsContextSupplier metricsContextSupplier) {
-        this.ticker = checkNotNull(ticker, "ticker must be provided");
-        this.timestampSupplier = checkNotNull(timestampSupplier, "timestampSupplier must be provided");
-        this.metricsEventPublisher = checkNotNull(metricsEventPublisher, "metricsEventPublisher must be provided");
-        this.metricsContextSupplier = checkNotNull(metricsContextSupplier, "metricsContextSupplier must be provided");
+        this.ticker = requireNonNull(ticker, "ticker must be provided");
+        this.timestampSupplier = requireNonNull(timestampSupplier, "timestampSupplier must be provided");
+        this.metricsEventPublisher = requireNonNull(metricsEventPublisher, "metricsEventPublisher must be provided");
+        this.metricsContextSupplier = requireNonNull(metricsContextSupplier, "metricsContextSupplier must be provided");
     }
 
     @Override
     public Promise<Response, NeverThrowsException> filter(Context context, Request request, Handler handler) {
         final Stopwatch stopwatch = Stopwatch.createStarted(ticker);
-        final Map<String, Object> routeMetricsContext = getRouteMetricsContext(context, request);
-        return handler.handle(context, request).thenOnResult(response -> {
-            try {
-                metricsEventPublisher.publish(buildRouteMetricsEvent(stopwatch, context, request, response, routeMetricsContext));
-            } catch (RuntimeException ex) {
-                logger.error("Failed to publish metrics due to exception", ex);
-            }
+        return getRouteMetricsContext(context, request).thenAsync(routeMetricsContext -> {
+            return handler.handle(context, request).thenOnResult(response -> {
+                try {
+                    metricsEventPublisher.publish(buildRouteMetricsEvent(stopwatch, context, request, response, routeMetricsContext));
+                } catch (RuntimeException ex) {
+                    logger.error("Failed to publish metrics due to exception", ex);
+                }
+            });
         });
     }
 
@@ -126,16 +129,18 @@ public class RouteMetricsFilter implements Filter {
         return metricEvent;
     }
 
-    private Map<String, Object> getRouteMetricsContext(Context context, Request request) {
+    private Promise<Map<String, Object>, NeverThrowsException> getRouteMetricsContext(Context context, Request request) {
         try {
-            final Map<String, Object> metricsContext = metricsContextSupplier.getMetricsContext(context, request);
-            if (metricsContext != null) {
-                return metricsContext;
-            }
+            return metricsContextSupplier.getMetricsContext(context, request)
+                                         .thenCatchRuntimeException(rte -> {
+                                             logger.error("Unexpected exception thrown invoking metricsContextSupplier", rte);
+                                             return Collections.emptyMap();
+                                         })
+                                         .then(metricsContext -> requireNonNullElse(metricsContext, Collections.emptyMap()));
         } catch (RuntimeException ex) {
             logger.error("Unexpected exception thrown invoking metricsContextSupplier", ex);
+            return Promises.newResultPromise(Collections.emptyMap());
         }
-        return Collections.emptyMap();
     }
 
     /**
