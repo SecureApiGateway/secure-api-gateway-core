d3fcf04b3fb936ab9109a800ce715e0736fb696a
bohocode <jamie.bowen@forgerock.com>
2024-10-09 17:02:57 +0100

    1546: Make getJson async - PR review comments
    Issue: https://github.com/SecureApiGateway/SecureApiGateway/issues/1546

--- config/7.3.0/fapi1part2adv/ig/scripts/groovy/ErrorResponseFilter.groovy
+++ config/7.3.0/fapi1part2adv/ig/scripts/groovy/ErrorResponseFilter.groovy
@@ -11,34 +11,29 @@ SCRIPT_NAME = "[ErrorResponseFilter] - "
 
 logger.debug(SCRIPT_NAME + "Running...")
 
-class ErrorResponse {
-    public String error_description
-    public String error
-}
-
 return next.handle(context, request).thenOnResult {
     response -> {
         if(response.status.isClientError()){
             logger.debug("{} Response has status {} - which is a Client Error", SCRIPT_NAME, response.status)
-            var errorResponse = response.entity.getJson();
-
-            logger.debug("{} Response has body {}", SCRIPT_NAME, errorResponse)
-            try {
-                ErrorResponse error = (ErrorResponse)errorResponse
-                if(error.error_description.equals("code_verifier parameter required")){
-                    logger.debug("{} changing error from {} to {}", SCRIPT_NAME, error.error, "invalid_grant")
-                    error.error = "invalid_grant"
-                    response.entity.setJson((Object)error)
-                } else
-
-                if (error.error_description.equals("The redirection URI provided does not match a pre-registered value.")){
-                    logger.debug("{} changing error from {} to {}", SCRIPT_NAME, error.error, "invalid_grant")
-                    error.error = "invalid_request_object"
-                    response.entity.setJson((Object)error)
+            response.entity.getJsonAsync().then(errorResponse -> {
+                logger.debug("{} Response has body {}", SCRIPT_NAME, errorResponse)
+                try {
+                    if(errorResponse.error_description.equals("code_verifier parameter required")){
+                        logger.debug("{} changing error from {} to {}", SCRIPT_NAME, errorResponse.error,
+                                "invalid_grant")
+                        errorResponse.error = "invalid_grant"
+                        response.entity.setJson(errorResponse)
+                    } else if (errorResponse.error_description.equals("The redirection URI provided does not match a " +
+                            "pre-registered value.")){
+                        logger.debug("{} changing error from {} to {}", SCRIPT_NAME, errorResponse.error,
+                                "invalid_grant")
+                        errorResponse.error = "invalid_request_object"
+                        response.entity.setJson(errorResponse)
+                    }
+                } catch (e){
+                    logger.debug("{} error casting json body to be ErrorResponse. Exception: {}" SCRIPT_NAME, e)
                 }
-            } catch (e){
-                logger.debug("{} error casting json body to be ErrorResponse. Exception: {}" SCRIPT_NAME, e)
-            }
+            })
         }
     }
 };
