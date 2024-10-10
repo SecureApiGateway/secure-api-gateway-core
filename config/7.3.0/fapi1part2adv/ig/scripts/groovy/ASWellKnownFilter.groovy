2e2890fff243a7810a39c514ddfd3a0b04356fd3
Dan Badham <dan.badham@forgerock.com>
2024-10-10 13:45:58 +0100

    1547: Use getJsonAsync in ASWellKnownFilter.groovy
    Fix ASWellKnownFilter.groovy to use getJsonAsync in order to avoid blocking.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1521

--- config/7.3.0/fapi1part2adv/ig/scripts/groovy/ASWellKnownFilter.groovy
+++ config/7.3.0/fapi1part2adv/ig/scripts/groovy/ASWellKnownFilter.groovy
@@ -1,16 +1,21 @@
+import static org.forgerock.util.promise.Promises.newResultPromise
+
 SCRIPT_NAME = "[ASWellKnownFilter] "
 logger.debug(SCRIPT_NAME + "Running...")
 
-next.handle(context, request).thenOnResult(response -> {
+next.handle(context, request).thenAsync(response -> {
     if (response.status.isSuccessful()) {
-        wellKnownData = response.entity.getJson()
-        // Configure auth methods supported using filter arg: tokenEndpointAuthMethodsSupported
-        wellKnownData["token_endpoint_auth_methods_supported"] = tokenEndpointAuthMethodsSupported
+        return response.entity.getJsonAsync().then(wellKnownData -> {
+                    // Configure auth methods supported using filter arg: tokenEndpointAuthMethodsSupported
+                    wellKnownData["token_endpoint_auth_methods_supported"] = tokenEndpointAuthMethodsSupported
 
-        // Update endpoints defined in mtlsEndpoints arg to use the mtls host
-        mtlsEndpoints.each { endpoint ->
-            wellKnownData[endpoint] = wellKnownData[endpoint].replace(igHost, mtlsHost)
-        }
-        response.entity.setJson(wellKnownData)
+                    // Update endpoints defined in mtlsEndpoints arg to use the mtls host
+                    mtlsEndpoints.each { endpoint ->
+                        wellKnownData[endpoint] = wellKnownData[endpoint].replace(igHost, mtlsHost)
+                    }
+                    response.entity.setJson(wellKnownData)
+                    return response
+        })
     }
+    return newResultPromise(response)
 })
